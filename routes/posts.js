d6f425a719f6ab9265ebed4d156cc01caa530d4b
Javier773 <javimoreno20@hotmail.com>
2023-06-16 10:59:43 +0200

    update

---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { addPost, deletePost, getPost, getPosts, getPostsRecon, updatePost } from "../controllers/post.js";
+
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/recon", getPostsRecon);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
+
+export default router
