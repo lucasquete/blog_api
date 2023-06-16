f410a5f58b944740b854443393cc09159a893b0f
Javier773 <javimoreno20@hotmail.com>
2023-06-16 11:19:59 +0200

    update

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
-import mysql from "mysql";
+import mysql2 from "mysql2";
 
-export const db = mysql.createPool({
-    connectionLimit : 10,
+export const db = mysql2.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_DATABASE,
     password: process.env.DB_USER,
