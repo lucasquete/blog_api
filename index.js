4e507d7e6a7d5844b8cbdc8cc0d15deb6de9fb7e
Javier773 <javimoreno20@hotmail.com>
2023-06-17 16:28:01 +0200

    update

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ dotenv.config();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({
+  limit: "50mb"
+}));
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'https://blog-drab-seven-35.vercel.app/client/public/uploads')
+      cb(null, './public/uploads')
     },
     filename: function (req, file, cb) {
       cb(null, Date.now()+file.originalname)
